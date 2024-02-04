4586cd846b5c7d6be9275b311043b7ebb21d65b6
chaiya srisawat <chaiya.88488@gmail.com>
2024-02-04 14:53:19 +0700

    .

--- contrib/core-operator/build_all.sh
+++ contrib/core-operator/build_all.sh
@@ -5,10 +5,10 @@ VERSION="v2.2.0"
 pushd .. 
 
 git checkout $VERSION
-docker build -t terrarebirth/core:$VERSION .
+docker build -t terrarebirth/core:$VERSION .  -f Dockerfile.core
 git checkout -
 
 popd
 
-docker build --build-arg version=$VERSION --build-arg chainid=paradise-1 -t terrarebirth/core-node:$VERSION .
-docker build --build-arg version=$VERSION --build-arg chainid=biden-1 -t terrarebirth/core-node:$VERSION-testnet .
+docker build --build-arg version=$VERSION --build-arg chainid=paradise-1 -t terrarebirth/core-node:$VERSION . -f Dockerfile.core
+docker build --build-arg version=$VERSION --build-arg chainid=biden-1 -t terrarebirth/core-node:$VERSION-testnet . -f Dockerfile.core
